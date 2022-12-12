6f802d109a7d7be203ae476f68b395609baeaf80
Max Gamel <gamelmagdy2.0@gmail.com>
2022-12-12 17:04:18 -0500

    part 10

--- models/index.js
+++ models/index.js
@@ -7,3 +7,6 @@ mongoose.connect(process.env.MONGO_URI, {
 })
 
 module.exports.Place = require('./Places')
+module.exports.Comment = require('./comment')
+
+
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,6 +2,30 @@ const React = require('react')
 const Def = require('../default')
 
 function show (data) {
+    let comments = (
+        <h3 className="inactive">
+            No comments yet!
+        </h3>
+    )
+
+if(data.place.comments.length) {
+     comments = data.place.comments.map(c => {
+        return (
+              <div className="border col-sm-4">
+                 <h2 className="rant">{ c.rant ? 'Rant!❌' : 'Rave!✅' }</h2>
+                 <h4>{ c.content }</h4>
+                    <h3>
+                     <strong>- { c.author }</strong>
+                    </h3>
+                 <h4>Rating: { c.stars }</h4>
+                 <form method="POST" action={ `/places/${data.place.id}/comment/${c.id}?_method=DELETE` }>
+                      <input type="submit" className="btn btn-danger" value="Delete Comment" />
+                 </form>
+              </div>
+            )
+        })
+ }
+
 return (
     <Def>
     <main>
